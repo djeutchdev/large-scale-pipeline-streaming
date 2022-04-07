8237b1b7446e75e3e88880a582ab1d456801a1ce
djeutchdev <djeutchdev@gmail.com>
2022-04-07 18:52:24 +0100

    OK

---
+++ src/test/scala/ProducerKafka.scala
@@ -0,0 +1,9 @@
+import org.apache.kafka.clients.producer._
+import org.apache.kafka.common.serialization._
+import org.apache.log4j.{LogManager,Logger}
+import java.util._
+import org.apache.kafka.common.security.auth.SecurityProtocol
+
+object ProducerKafka extends App{
+
+}
